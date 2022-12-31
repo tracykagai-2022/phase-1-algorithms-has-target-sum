6e08ae741a7b9c4ea1e224052ff91d4f34cc8583
tracykagai-2022 <118049185+tracykagai-2022@users.noreply.github.com>
2022-12-31 20:22:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+const chai = require("chai");
+global.expect = chai.expect;
+const hasTargetSum = require("../index");
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
+describe("hasTargetSum", () => {
+  it("returns true when the array is [3, 8, 12, 4, 11, 7] and the target is 10", () => {
+    expect(hasTargetSum([3, 8, 12, 4, 11, 7], 10)).to.be.true;
+  });
+  it("returns true when the array is [22, 19, 4, 6, 30] and the target is 25", () => {
+    expect(hasTargetSum([22, 19, 4, 6, 30], 25)).to.be.true;
+  });
+  it("returns false when the array is [1, 2, 5] and the target is 4", () => {
+    expect(hasTargetSum([1, 2, 5], 4)).to.be.false;
+  });
+  it("returns true when the array is [-7, 10, 4, 8] and the target is 3", () => {
+    expect(hasTargetSum([-7, 10, 4, 8], 3)).to.be.true;
+  });
+  it("returns true when the array is [1, 2, 3, 4] and the target is 5", () => {
+    expect(hasTargetSum([1, 2, 3, 4], 5)).to.be.true;
+  });
+  it("returns true when the array is [2, 2, 3, 3] and the target is 4", () => {
+    expect(hasTargetSum([2, 2, 3, 3], 4)).to.be.true;
+  });
+  it("returns false when the array is [4] and the target is 4", () => {
+    expect(hasTargetSum([4], 5)).to.be.false;
+  });
+});
